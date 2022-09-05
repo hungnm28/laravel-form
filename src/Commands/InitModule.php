0d401da3fa0c54e0e065af33ccdb077ad4226b9d
Hung Nguyen Manh <hungnm@Hungs-MacBook-Pro.local>
2022-09-05 17:34:50 +0700

    fix layout

--- src/Commands/InitModule.php
+++ src/Commands/InitModule.php
@@ -10,7 +10,7 @@ class InitModule extends Command
 {
     use WithCommandTrait;
 
-    protected $signature = 'lf:init-module {name} {--all} {--icon} {--mix} {--layout} {--provider} {--route} {--gitignore}';
+    protected $signature = 'lf:init-module {name} {--all} {--mix} {--layout} {--provider} {--route} {--gitignore}';
 
     protected $description = 'Init module: ';
 
@@ -34,9 +34,6 @@ class InitModule extends Command
         }
         if($this->option("route") || $this->option("all")){
             $this->call('lf:init-route', ['name' => $name]);
-        }
-        if($this->option("icon") || $this->option("all")){
-            $this->call('lf:init-icon');
         }
         if($this->option("gitignore") || $this->option("all")){
             $this->call('lf:init-gitignore',['name' => $name]);
