a9c34f017b91550b0cd8179ab04cccff62af8eb3
Hung Nguyen Manh <hungnm@Hungs-MacBook-Pro.local>
2022-09-07 15:09:00 +0700

    f

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,7 +1,10 @@
-@props(["name","label"=>null,"class"=>null,"params"=>[]])
+@props(["name","label"=>null,"class"=>null,"params"=>[],"default"=>[]])
 
 <x-lf.form.field :name="$name" :label="$label" :class="$class">
     <select class="form-input" wire:model="{{$name}}" id="lf-{{$name}}" {{$attributes}} >
+        @foreach($default as $k => $val)
+            <option value="{{$k}}">{{$val}}</option>
+        @endforeach
         @foreach($params as $k => $param)
             <option value="{{$k}}">{{$param}}</option>
         @endforeach
