f8440d4eb4def00371449330c9aa6c1de42ca864
Hung Nguyen Manh <hungnm@Hungs-MacBook-Pro.local>
2022-11-23 16:50:04 +0700

    f

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -5,10 +5,17 @@ use Illuminate\Support\Str;
 if (!function_exists('lfIcon')) {
     function lfIcon($name, $width = 18, $height = 0, $viewBox = "0 0 24 24", $attribute = null)
     {
+        $version = intval(config('app.asset_version'));
+        $env = config("app.env","production");
+        $asset = "/assets";
+        if($env == "production" && $version>0){
+            $asset .= "/$version";
+        }
+
         if ($height == 0) $height = $width;
         return '
             <svg width="' . $width . 'px" height="' . $height . 'px"  viewBox="' . $viewBox . '" class="mcon" ' . $attribute . ' fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <use xlink:href="' . env('ASSET_ICON', '/assets') . '/images/icons.svg#' . $name . '"/>
+                <use xlink:href="'.$asset.'/images/icons.svg#' . $name . '"/>
             </svg>
         ';
     }
